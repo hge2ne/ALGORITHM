5635a7cc3bea54b353ba80652dec5b0e34939b5d
subillie <ddubi701@gmail.com>
2025-07-29 17:12:10 +0900

    3000

---
+++ python_practice/Data stuructures2/3000.py
@@ -0,0 +1,35 @@
+#문제
+data = [
+    {
+        'name': 'galxy flip',
+        'company': 'samsung',
+        'is_collapsible': True,
+    },
+    {
+        'name': 'ipad',
+        'is_collapsible': False
+    },
+    {
+        'name': 'galxy fold',
+        'company': 'samsung',
+        'is_collapsible': True
+    },
+    {
+        'name': 'galxy note',
+        'company': 'samsung',
+        'is_collapsible': False
+    },
+    {
+        'name': 'optimus',
+        'is_collapsible': False
+    },
+]
+
+key_list = ['name', 'company', 'is_collapsible']
+
+# 아래에 코드를 작성하시오.
+
+for item in data: 
+    for key in key_list:
+        item.setdefault(key,'unknown')
+        print(f'{key}은/는 {item.get(key)}입니다.')
